54af79bf0dd4e108611fcc1098f97a9f551b4f4d
jiangzheng <nightjiang1997>
2022-06-30 14:22:33 +0800

    1. modify the test package strcture, 2. modify the string returned by MapperProxy.invoke method

---
+++ src/test/java/com/exmaple/small/mybatis/binding/MapperProxyFactoryTests.java
@@ -0,0 +1,2 @@
+package com.exmaple.small.mybatis.binding;public class MapperProxyFactoryTests {
+}
---
+++ src/test/java/com/exmaple/small/mybatis/binding/mapper/UserMapper.java
@@ -0,0 +1,7 @@
+package com.exmaple.small.mybatis.binding;
+// User Mapper For Test
+public interface UserMapper {
+  String findById(String id);
+
+  String findAll();
+}
