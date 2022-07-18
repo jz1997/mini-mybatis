c0d81a666ba5778b07465dced234eb3d5bb3c78a
jiangzheng <nightjiang1997>
2022-07-18 22:10:25 +0800

    Remove unsed reference.

--- src/main/java/com/exmaple/small/mybatis/binding/MapperMethod.java
+++ src/main/java/com/exmaple/small/mybatis/binding/MapperMethod.java
@@ -1,24 +1,20 @@
 package com.exmaple.small.mybatis.binding;
 
-import com.exmaple.small.mybatis.reflection.ParamNameResolver;
 import com.exmaple.small.mybatis.session.Configuration;
 import com.exmaple.small.mybatis.session.SqlSession;
 
 import java.lang.reflect.Method;
 
 public class MapperMethod {
-    private SqlCommandType sqlCommandType;
-    private String statementId;
-    private Method method;
-
-    private ParamNameResolver paramNameResolver;
+    private final SqlCommandType sqlCommandType;
+    private final String statementId;
+    private final Method method;
 
     public MapperMethod(Class<?> mapperInterface, Method method, Configuration configuration) {
         this.statementId = mapperInterface.getName() + "." + method.getName();
         MappedStatement mappedStatement = configuration.getMappedStatement(statementId);
         this.sqlCommandType = mappedStatement.getSqlCommandType();
         this.method = method;
-        this.paramNameResolver = new ParamNameResolver(method);
     }
 
     public Object execute(SqlSession sqlSession, Object[] args) {
